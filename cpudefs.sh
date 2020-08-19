e6022f3a140388de68738842e40d7da5627c5ba2
Koichi Murase <myoga.murase@gmail.com>
2020-08-19 09:00:46 +0900

    cpudefs (rsh.dispatch): add default for localhost

--- cpudefs.sh
+++ cpudefs.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 function rsh.dispatch {
-  local host="$1"; shift
+  local host=$1; shift
   local command="$*"
 
-  local handler="$cpudir/hosts/$host.sh"
+  local handler=$cpudir/hosts/$host.sh
   if [[ -s $handler ]]; then
     source "$handler" rsh "bash" <<< "$command" && return
   fi
+
+  case $host in
+  (localhost|127.0.0.1|::1|"${HOSTNAME%%.*}")
+    handler=$cpudir/hosts/sh
+    source "$handler" rsh "bash" <<< "$command" && return
+  esac
 
   rsh "$host" "cd $cpudir; bash" <<< "$command"
 }
